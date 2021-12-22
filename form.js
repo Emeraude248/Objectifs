cb9960a03ed19e3ed5afaa79b8764cda61ad5ea4
Emeraude248 <96231220+Emeraude248@users.noreply.github.com>
2021-12-22 09:46:09 +0100

    début js
    difficultés mise enforme flexbox + message d'erreur

---
+++ form.js
@@ -0,0 +1,29 @@
+// Il y a plusieurs façon de sélectionner un nœud DOM ; ici on récupère
+// le formulaire et le champ d'e-mail ainsi que l'élément span
+// dans lequel on placera le message d'erreur
+
+var form  = document.getElementsByTagName('form')[0];
+var email = document.getElementById('email');
+var error = document.querySelector('.error');
+
+email.addEventListener("input", function (event) {
+
+  if (email.validity.valid) {
+    // S'il y a un message d'erreur affiché et que le champ
+    // est valide, on retire l'erreur
+    error.innerHTML = ""; // On réinitialise le contenu
+    error.className = "error"; // On réinitialise l'état visuel du message
+  }
+}, false);
+form.addEventListener("submit", function (event) {
+  // Chaque fois que l'utilisateur tente d'envoyer les données
+  // on vérifie que le champ email est valide.
+  if (!email.validity.valid) {
+
+    // S'il est invalide, on affiche un message d'erreur personnalisé
+    error.innerHTML = "L'adresse mail n'est pas valide";
+    error.className = "error active";
+    // Et on empêche l'envoi des données du formulaire
+    event.preventDefault();
+  }
+}, false);
